03c599755d1c4af3682bf497257f889a26e13c8d
Jacky014 <88843131+Jacky014@users.noreply.github.com>
2022-09-06 16:47:38 -0500

    agregando cambios

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
 
-function edad (){
-let year
-let edad
-year=parseInt(prompt("¿En qué año naciste?"))
-
-edad = 2022 - year
-
+let year = parseInt(prompt("Ingrese su año de nacimiento"));
+edad = 2022 - year; 
 if (edad <= 17){
     alert("Eres MENOR de edad porque tienes " +edad+" años, no podemos venderte helados, vuelve en unos años!")
 }
 
 else{
     alert("Eres MAYOR de edad porque tienes " +edad+" años, estamos listos para tomar tu pedido")
+    
 
-}
 }
